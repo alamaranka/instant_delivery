730b4b476438a91d66f8366fe011852b463587d4
Ali Pala <alipala@buffalo.edu>
2021-10-22 00:59:39 +0300

    relocate operator implementation added.

--- local_search.py
+++ local_search.py
@@ -2,7 +2,7 @@ from helpers import *
 from data.model import Route
 
 
-def two_opt(data, route):
+def two_opt(data, route, output_log):
     for i in range(len(route.job_ids) - 1):
         for j in range(i + 1, len(route.job_ids)):
             new_job_order = []
@@ -16,7 +16,8 @@ def two_opt(data, route):
                 improvement = route.delivery_duration - new_route.delivery_duration
                 route.job_ids = new_route.job_ids
                 route.delivery_duration = new_route.delivery_duration
-                print('Two-Opt operator improvement\t-> {0}'.format(improvement))
+                if output_log:
+                    print('Two-Opt operator improvement\t-> {0}'.format(improvement))
                 return True
     return False
 
@@ -25,7 +26,7 @@ def apply_two_opt(data, routes):
     for route in routes:
         improved = True
         while improved:
-            improved = two_opt(data, route)
+            improved = two_opt(data, route, True)
 
 
 def exchange(data, routes):
@@ -79,6 +80,56 @@ def apply_exchange(data, routes):
         improved = exchange(data, routes)
 
 
+def relocate(data, routes):
+    for r1 in range(len(routes)):
+        for r2 in range(len(routes)):
+            if r1 != r2:
+                route1 = routes[r1]
+                route2 = routes[r2]
+                for j1 in range(len(route1.job_ids)):
+                    for j2 in range(len(route2.job_ids)):
+                        # get job to relocate
+                        job1 = [v for v in data.jobs if v.id == route1.job_ids[j1]][0]
+                        # create copies of routes
+                        route1_copy = route1.copy()
+                        route2_copy = route2.copy()
+                        # remove job from its current route
+                        job1_index = route1_copy.job_ids.index(job1.id)
+                        route1_copy.job_ids.remove(job1.id)
+                        route1_copy.load -= job1.delivery
+                        # prepare new routes
+                        # insert job1 to route2
+                        route2_copy = insert_job_to_route(data, route2_copy, 0, job1)
+                        # apply two-opt on new route
+                        improved = True
+                        while improved:
+                            improved = two_opt(data, route2_copy, False)
+                        # get necessary info
+                        new_total_delivery_duration = route1_copy.delivery_duration + route2_copy.delivery_duration
+                        current_total_delivery_duration = route1.delivery_duration + route2.delivery_duration
+                        route1_vehicle_capacity = [v for v in data.vehicles if v.id == route1.vehicle_id][0].capacity
+                        route2_vehicle_capacity = [v for v in data.vehicles if v.id == route2.vehicle_id][0].capacity
+                        if new_total_delivery_duration < current_total_delivery_duration and \
+                                route1_copy.load <= route1_vehicle_capacity and \
+                                route2_copy.load <= route2_vehicle_capacity:
+                            improvement = current_total_delivery_duration - new_total_delivery_duration
+                            route1.job_ids = route1_copy.job_ids
+                            route1.delivery_duration = route1_copy.delivery_duration
+                            route1.load = route1_copy.load
+                            route2.job_ids = route2_copy.job_ids
+                            route2.delivery_duration = route2_copy.delivery_duration
+                            route2.load = route2_copy.load
+                            print('Relocate operator improvement\t-> {0}'.format(improvement))
+                            return True
+    return False
+
+
+def apply_relocate(data, routes):
+    improved = True
+    while improved:
+        improved = relocate(data, routes)
+
+
 def local_search(data, solution):
     improved = True
     while improved:
@@ -86,6 +137,7 @@ def local_search(data, solution):
         total_delivery_duration = solution.total_delivery_duration
         apply_two_opt(data, solution.routes)
         apply_exchange(data, solution.routes)
+        apply_relocate(data, solution.routes)
         solution.update_duration()
         improved |= solution.total_delivery_duration < total_delivery_duration
     solution.update_duration()
--- main.py
+++ main.py
@@ -4,33 +4,36 @@ from data.model import Solution, Route
 from helpers import *
 from local_search import *
 
-# starting algorithm
-start_time = time.time()
+if __name__ == "__main__":
 
-# reading data
-data = read.data
+    # starting algorithm
+    start_time = time.time()
 
-# initial solution
-solution = Solution()
-solution.populate(data.vehicles)
-for job in data.jobs:
-    closest_vehicle = get_closest_vehicle(solution.routes, data.vehicles, data.matrix, job)
-    route = [x for x in solution.routes if x.vehicle_id == closest_vehicle.id][0]
-    route.job_ids.append(job.id)
-    route.load += job.delivery
-for route in solution.routes:
-    route.update_duration(data)
-solution.update_duration()
-print('Total delivery duration of the initial solution: {0}'
-      .format(solution.total_delivery_duration))
+    # reading data
+    data = read.data
 
-# improve routes with local search
-solution = local_search(data, solution)
-print('Total delivery duration after local search: {0}'
-      .format(solution.total_delivery_duration))
+    # initial solution
+    solution = Solution()
+    solution.populate(data.vehicles)
+    for job in data.jobs:
+        closest_vehicle = get_closest_vehicle(solution.routes, data.vehicles, data.matrix, job)
+        route = [x for x in solution.routes if x.vehicle_id == closest_vehicle.id][0]
+        route.job_ids.append(job.id)
+        route.load += job.delivery
+    for route in solution.routes:
+        route.update_duration(data)
+    solution.update_duration()
+    print('Total delivery duration of the initial solution: {0}'
+          .format(solution.total_delivery_duration))
 
-# export solution
-solution.export('output.json')
+    # improve routes with local search
+    solution = local_search(data, solution)
+    print('Total delivery duration after local search: {0}'
+          .format(solution.total_delivery_duration))
 
-# algorithm completed
-print("Algorithm completed in %.3f seconds." % (time.time() - start_time))
+    # export solution
+    solution.export('output.json')
+
+    # algorithm completed
+    print("Algorithm completed in %.3f seconds." %
+          (time.time() - start_time))
